e82f121853f14dc99964ba0c831b4a6d4e87b93b
Aline Galvão <alinegs93@gmail.com>
2022-11-19 14:54:39 -0300

    inclusão do Perfil na rota

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,16 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import Perfil from '../pages/Perfil';
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-  ]);
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
 
-  export default router;
+  {
+    path: "/perfil/:id",
+    element: <Perfil />
+  },
+]);
+
+export default router;
