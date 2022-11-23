585ec480126f4ef8ae82de761edae939e66e94d7
André Lima <109320300+andreolimao@users.noreply.github.com>
2022-11-23 02:25:35 -0300

    subindo pagina profissionais vet
    primeira versão, falta ajustes

---
+++ src/pages/Profissionais.style.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const CardProf1 = styled.div`
+    flex-direction: column;
+    display: flex;
+    justify-content: center; 
+    align-items: center;
+
+    background-color: #FFFFFF;
+    width: 525px;
+    height: 260px;
+    border-radius: 25px;
+
+    margin-right: 2rem;
+
+    flex: none;
+
+    color: #0E403F;
+    img{
+        margin-bottom: 0.5rem;
+    }
+    h4{
+        font-weight: 800;
+        font-size: 25px;
+        line-height: 25px;
+        margin-bottom: 0.5rem;
+    }
+    
+    div #info_valor{
+    display: flex;
+    flex-direction: row-reverse;
+    margin: 50px;
+    padding: 30px;
+
+    }
+
+        
---
+++ src/pages/Profissionais.tsx
@@ -0,0 +1,253 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import ImputFiltro from "../components/ImputFiltro";
+import { CardProf1 } from "./Profissionais.style";
+import icon from "../../public/foto.svg";
+import EstrelaCard from "./../../../public/EstrelaCard.svg"
+import Footer from "../components/Footer";
+import ButtonWhatsap from "../components/ButtonWhatsap";
+
+function Profissional() { 
+    return(
+        <>
+            <NavBar />
+            <ImputFiltro />
+            
+            <CardProf1>
+                <div><img src={icon} alt="foto-profissional" /></div>
+                <h4>Adriana Pacheco</h4>
+                <div>
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+            </div>
+
+                <h4>Clínica Geral</h4>
+                <h4>Rio de Janeiro - RJ</h4>
+
+            <div className="info_valor">
+                        <h4>Valor da Consulta</h4>
+                        <p>Horário comercial: R$ 100</p>
+                        <p>Urgências 24h: R$ 140</p>
+                        <ButtonWhatsap/>
+            </div>
+            <CardProf1/>
+
+
+            <CardProf1>
+                <div><img src={icon} alt="foto-profissional" /></div>
+                <h4>Silvia Oliveira</h4>
+                <div>
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+            </div>
+
+                <h4>Clínica Geral</h4>
+                <h4>Salvador - BA </h4>
+
+            <div className="info_valor">
+                        <h4>Valor da Consulta</h4>
+                        <p>Horário comercial: R$ 90</p>
+                        <p>Urgências 24h: R$ 140</p>
+                        <ButtonWhatsap/>
+            </div>
+            <CardProf1/>
+
+
+            <CardProf1>
+                <div><img src={icon} alt="foto-profissional" /></div>
+                <h4>Marcela Falcão</h4>
+                <div>
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+            </div>
+
+                <h4>Clínica Geral e Cirurgia</h4>
+                <h4> Manaus - AM </h4>
+
+            <div className="info_valor">
+                        <h4>Valor da Consulta</h4>
+                        <p>Horário comercial: R$ 100</p>
+                        <p>Urgências 24h: R$ 150</p>
+                        <ButtonWhatsap/>
+            </div>
+            <CardProf1/>
+
+
+            <CardProf1>
+                <div><img src={icon} alt="foto-profissional" /></div>
+                <h4>Fabio Barreto</h4>
+                <div>
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+            </div>
+
+                <h4>Clínica Geral e Cirurgia</h4>
+                <h4>Brasilia - DF </h4>
+
+            <div className="info_valor">
+                        <h4>Valor da Consulta</h4>
+                        <p>Horário comercial: R$ 95</p>
+                        <p>Urgências 24h: R$ 120</p>
+                        <ButtonWhatsap/>
+            </div>
+            <CardProf1/>
+
+
+            <CardProf1>
+                <div><img src={icon} alt="foto-profissional" /></div>
+                <h4>Gustavo Marcelino</h4>
+                <div>
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+            </div>
+
+                <h4>Clínica Geral e Anestesiologia</h4>
+                <h4>Maceió - AL </h4>
+
+            <div className="info_valor">
+                        <h4>Valor da Consulta</h4>
+                        <p>Horário comercial: R$ 100</p>
+                        <p>Urgências 24h: R$ 130</p>
+                        <ButtonWhatsap/>
+            </div>
+            <CardProf1/>
+
+
+            <CardProf1>
+                <div><img src={icon} alt="foto-profissional" /></div>
+                <h4>Caio Araujo</h4>
+                <div>
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+            </div>
+
+                <h4>Clínica Geral e Acupuntura</h4>
+                <h4>Curitiba - PR </h4>
+
+            <div className="info_valor">
+                        <h4>Valor da Consulta</h4>
+                        <p>Horário comercial: R$ 100</p>
+                        <p>Urgências 24h: R$ 140</p>
+                        <ButtonWhatsap/>
+            </div>
+            <CardProf1/>
+
+
+            <CardProf1>
+                <div><img src={icon} alt="foto-profissional" /></div>
+                <h4>Carolina Veloso</h4>
+                <div>
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+            </div>
+
+                <h4>Clínica Geral</h4>
+                <h4>Recife - PE </h4>
+
+            <div className="info_valor">
+                        <h4>Valor da Consulta</h4>
+                        <p>Horário comercial: R$ 100</p>
+                        <p>Urgências 24h: R$ 140</p>
+                        <ButtonWhatsap/>
+            </div>
+            <CardProf1/>
+
+
+            <CardProf1>
+                <div><img src={icon} alt="foto-profissional" /></div>
+                <h4>Aline Dias</h4>
+                <div>
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+            </div>
+
+                <h4>Clínica Geral</h4>
+                <h4>Betim - MG </h4>
+
+            <div className="info_valor">
+                        <h4>Valor da Consulta</h4>
+                        <p>Horário comercial: R$ 90</p>
+                        <p>Urgências 24h: R$ 140</p>
+                        <ButtonWhatsap/>
+            </div>
+            <CardProf1/>
+
+
+            <CardProf1>
+                <div><img src={icon} alt="foto-profissional" /></div>
+                <h4>Priscila Vieira</h4>
+                <div>
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+            </div>
+
+                <h4>Clínica Geral</h4>
+                <h4>São Paulo - SP </h4>
+
+            <div className="info_valor">
+                        <h4>Valor da Consulta</h4>
+                        <p>Horário comercial: R$ 100</p>
+                        <p>Urgências 24h: R$ 140</p>
+                        <ButtonWhatsap/>
+            </div>
+            <CardProf1/>
+
+
+            <CardProf1>
+                <div><img src={icon} alt="foto-profissional" /></div>
+                <h4>Felipe Carvalho</h4>
+                <div>
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+                <img src={EstrelaCard} alt="Estrela de avaliação" />
+            </div>
+
+                <h4>Dermatologia</h4>
+                <h4>Anápolis - GO</h4>
+
+            <div className="info_valor">
+                        <h4>Valor da Consulta</h4>
+                        <p>Horário comercial: R$ 100</p>
+                        <p>Urgências 24h: R$ 150</p>
+                        <ButtonWhatsap/>
+            </div>
+            <CardProf1/>
+
+
+            <Footer/>
+
+            </>
+    );
+}
+
+export default Profissional; 
