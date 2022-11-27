f9443aa3865b5a81a14790f95cd15dca060aca95
Aline Galvão <alinegs93@gmail.com>
2022-11-27 01:00:16 -0300

    parâmetros por id e mudança de rota

--- src/pages/Perfil.tsx
+++ src/pages/Perfil.tsx
@@ -5,59 +5,89 @@ import { Container, Title, ConsultaValor, AboutMe } from "./Perfil.style";
 import icon from "../../public/foto.svg";
 import Footer from "../components/Footer";
 import ButtonWhatsap from "../components/ButtonWhatsap";
+import { useDispatch } from "react-redux";
+import api from "../services/api";
+import CardProfissionalVet, { CardVetsProps } from "../components/CardProfissionalVet";
+import EstrelaCard from "./../../../public/EstrelaCard.svg"
+
 
 function Perfil() {
+
+    const { id } = useParams();
+    const [isLoading, setIsLoading] = useState(true);
+    const [vetsData, setVetsData] = useState<CardProfissionalVet>({} as CardVetsProps);;
+
+    async function getVetsData() {
+        const { data } = await api.get("vets/" + id);
+
+        setVetsData({
+            id: data.userId,
+            avatar: data.avatar,
+            name: data.name,
+            crmv: data.crmv,
+            specialty: data.specialty,
+            queryValue: data.queryValue,
+            queryDutyValue: data.queryDutyValue,
+            formation: data.formation,
+            experience: data.experience,
+            aboutMe: data.aboutMe,
+        });
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        getVetsData();
+    }, []);
+
+    if (isLoading) {
+        return <p>Carregando</p>;
+    }
     return (
         <>
             <NavBar />
             <Container>
                 <div className="foto__perfil">
-                    <img src={icon} alt="foto-perfil" />
+                    <img src={vetsData.avatar} alt="foto-perfil" />
                 </div>
-                
+
                 <div className="perfil__vet">
-                    <Title>Adriana Pacheco</Title>
+                    <Title>{vetsData.name}</Title>
                     <span>
-                        CRMV: 635375
+                        CRMV: {vetsData.crmv}
                     </span>
                     <h4>
-                        Especialidade: <span>Clínica Geral</span>
+                        Especialidade: <span>{vetsData.specialty}</span>
                     </h4>
                     <h4>
-                        Endereço de Atendimento: 
+                        Endereço de Atendimento:
                         <span>
-                            Av. das Américas, 1100, Empresarial Center Master, sala 302 -
-                            Barra da Tijuca, Rio de Janeiro - RJ.
+
+
                         </span>
                     </h4>
-                    
+
                 </div>
-            
-            <ConsultaValor>
-            <div className="info__valor">
+
+                <ConsultaValor>
+                    <div className="info__valor">
                         <h4>Valor da Consulta</h4>
                         <p>Horário comercial: R$ 100</p>
                         <p>Urgências 24h: R$ 140</p>
-                        <ButtonWhatsap/>
+                        <ButtonWhatsap />
                     </div>
-                    </ConsultaValor>
-                    </Container>
+                </ConsultaValor>
+            </Container>
             <AboutMe>
                 <h2>Sobre mim</h2>
                 <p>
-                    A Dra. Adriana Pacheco se formou na Escola de Medicina Veterinária da
-                    Universidade Federal do Rio de <br />Janeiro. Dra. Adriana tem um interesse
-                    especial em medicina felina e medicina interna. Ela tem 2 gatos <br />em
-                    casa e gosta de andar de bicicleta ao ar livre e viajar.
+                    {vetsData.aboutMe}
                 </p>
                 <h2>Experiência</h2>
                 <p>
-                    Experiência diversificada, trabalhando em medicina de pequenos
-                    animais, medicina veterinária de emergência <br />e medicina veterinária de
-                    saúde pública desde 2010.
+                    {vetsData.experience} <br/>
                 </p>
                 <h2>Formação</h2>
-                <p>UFRJ - Universidade Federal do Rio de Janeiro.</p>
+                <p>{vetsData.formation}</p>
             </AboutMe>
             <Footer />
         </>
