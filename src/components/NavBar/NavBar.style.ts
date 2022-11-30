d5b120f9983305e47ee31e23aa736a9db25d2c86
Aline Galvão <alinegs93@gmail.com>
2022-11-30 16:10:05 -0300

    Revert "deletando arquivos"
    This reverts commit 210df483abb868375d282f0807d82f2a74278cfc.

---
+++ src/components/NavBar/NavBar.style.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+    width: 100%;
+    height: 15vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #017369;
+
+    div {
+        
+        margin-left: 5%;
+
+        a {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-decoration: none;
+        }
+
+        h2 {
+            color: #fff;
+            font-size: 2.5rem;
+            margin-left: 5%;
+        }
+    }
+
+    nav {
+            display: flex;
+            align-items: center;
+            margin-right: 15%;
+
+            .navegacaoNavBat {
+                display: flex;
+                align-items: center;
+                color: #fff;
+                text-decoration: none;
+                padding: 0 3%;
+                white-space: nowrap;
+                font-weight: 600;
+            }
+        }
+`;
